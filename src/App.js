0922c2c217939f9565ce67dc66692ecda7e6c6dd
Your Name <you@example.com>
2021-06-12 23:42:35 -0700

    added new styles for this pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Layout from './components/Layout/Layout'
+import AltynKumara from './components/AltynKumara/AltynKumara'
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <AltynKumara/>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
